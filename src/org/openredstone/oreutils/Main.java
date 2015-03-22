2afb905b98ef899cf583b8b7d1e85e57b4505f63
mort <martid0311@gmail.com>
2015-03-22 05:11:20 +0100

    Can now print hello world.

---
+++ src/org/openredstone/oreutils/Main.java
@@ -0,0 +1,11 @@
+package org.openredstone.oreutils;
+
+import static java.lang.System.out;
+
+public class Main
+{
+	public static void main(final String[] arguments)
+	{
+		out.println("Hello World!");
+	}
+}
